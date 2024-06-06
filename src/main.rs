be3a201949cef156e60d6f07c5a808e12a9dbe22
Stephane Benoit <stefb@wizzz.net>
2024-06-06 16:37:35 +0200

    rewrite

--- src/main.rs
+++ src/main.rs
@@ -1,74 +1,155 @@
-use warp::Filter;
-use std::net::UdpSocket;
-use serde::{Deserialize, Serialize};
-use log::{info, error};
-use simplelog::{Config, SimpleLogger, LevelFilter};
+use hyper::service::{make_service_fn, service_fn};
+use hyper::{Body, Request, Response, StatusCode, Server};
+use regex::Regex;
+use serde::Serialize;
+use std::collections::HashMap;
+use std::net::SocketAddr;
+use std::sync::{Arc, Mutex};
+use tokio::net::UdpSocket;
+use base64::decode;
+use std::env;
 
-#[derive(Deserialize, Serialize)]
-struct WebhookRequest {
-    mac: String,
+#[derive(Serialize)]
+struct StatusResponse {
+    status: String,
+}
+
+struct AppState {
+    machine_status: Mutex<HashMap<String, String>>,
+    broadcast_ip: String,
+    broadcast_port: u16,
+    username: Option<String>,
+    password: Option<String>,
+    token: Option<String>,
+}
+
+async fn handle_request(req: Request<Body>, state: Arc<AppState>) -> Result<Response<Body>, hyper::Error> {
+    let get_regex = Regex::new(r"^/(?P<MAC>([\da-f]{2}[:-]){5}[\da-f]{2})[/]?$").unwrap();
+    let post_regex = Regex::new(r"^/(?P<MAC>([\da-f]{2}[:-]){5}[\da-f]{2})/\?op=(?P<OP>(start|stop))$").unwrap();
+
+    let auth_header = req.headers().get("Authorization").and_then(|h| h.to_str().ok());
+
+    if let Some(auth) = &state.token {
+        if auth_header != Some(auth) {
+            return Ok(Response::builder()
+                .status(StatusCode::UNAUTHORIZED)
+                .body(Body::from("Unauthorized!\n"))
+                .unwrap());
+        }
+    } else if let (Some(username), Some(password)) = (&state.username, &state.password) {
+        if let Some(auth) = auth_header {
+            let cred = decode(auth.split_whitespace().nth(1).unwrap_or("")).unwrap();
+            let cred_str = String::from_utf8(cred).unwrap();
+            if cred_str != format!("{}:{}", username, password) {
+                return Ok(Response::builder()
+                    .status(StatusCode::UNAUTHORIZED)
+                    .body(Body::from("Unauthorized!\n"))
+                    .unwrap());
+            }
+        } else {
+            return Ok(Response::builder()
+                .status(StatusCode::UNAUTHORIZED)
+                .body(Body::from("Unauthorized!\n"))
+                .unwrap());
+        }
+    }
+
+    if req.method() == hyper::Method::GET {
+        if let Some(caps) = get_regex.captures(req.uri().path()) {
+            let mac = caps.name("MAC").unwrap().as_str().to_string();
+            let status = {
+                let machine_status = state.machine_status.lock().unwrap();
+                machine_status.get(&mac).cloned().unwrap_or_else(|| "unknown".to_string())
+            };
+
+            let response = StatusResponse { status };
+            let body = serde_json::to_string(&response).unwrap() + "\n";
+            Ok(Response::new(Body::from(body)))
+        } else {
+            Ok(Response::builder()
+                .status(StatusCode::BAD_REQUEST)
+                .body(Body::from("Unknown path!\n"))
+                .unwrap())
+        }
+    } else if req.method() == hyper::Method::POST {
+        if let Some(caps) = post_regex.captures(req.uri().path()) {
+            let mac = caps.name("MAC").unwrap().as_str().to_string();
+            let op = caps.name("OP").unwrap().as_str();
+
+            if op == "start" {
+                let mac_address = mac.replace(":", "").replace("-", "");
+                let mut packet = vec![0xFF; 6];
+                for _ in 0..16 {
+                    packet.extend_from_slice(&hex::decode(&mac_address).unwrap());
+                }
+
+                let broadcast_addr = format!("{}:{}", state.broadcast_ip, state.broadcast_port);
+                let socket = UdpSocket::bind("0.0.0.0:0").await.unwrap();
+                socket.set_broadcast(true).unwrap();
+                socket.send_to(&packet, &broadcast_addr).await.unwrap();
+
+                {
+                    let mut machine_status = state.machine_status.lock().unwrap();
+                    machine_status.insert(mac, "running".to_string());
+                }
+
+                Ok(Response::new(Body::from("WoL packet sent!\n")))
+            } else if op == "stop" {
+                {
+                    let mut machine_status = state.machine_status.lock().unwrap();
+                    machine_status.insert(mac, "stopped".to_string());
+                }
+
+                Ok(Response::new(Body::from("User has shutdown the system!\n")))
+            } else {
+                Ok(Response::builder()
+                    .status(StatusCode::BAD_REQUEST)
+                    .body(Body::from("Unknown operation!\n"))
+                    .unwrap())
+            }
+        } else {
+            Ok(Response::builder()
+                .status(StatusCode::BAD_REQUEST)
+                .body(Body::from("Unknown path!\n"))
+                .unwrap())
+        }
+    } else {
+        Ok(Response::builder()
+            .status(StatusCode::METHOD_NOT_ALLOWED)
+            .body(Body::from("Method not allowed!\n"))
+            .unwrap())
+    }
 }
 
 #[tokio::main]
 async fn main() {
-    // Initialiser le logger
-    SimpleLogger::init(LevelFilter::Info, Config::default()).unwrap();
+    let broadcast_ip = env::args().nth(1).unwrap_or("255.255.255.255".to_string());
+    let broadcast_port: u16 = env::args().nth(2).unwrap_or("9".to_string()).parse().unwrap();
+    let port: u16 = env::args().nth(3).unwrap_or("8080".to_string()).parse().unwrap();
+    let username = env::args().nth(4);
+    let password = env::args().nth(5);
+    let token = env::args().nth(6);
 
-    let webhook = warp::path("webhook")
-        .and(warp::post())
-        .and(warp::body::json())
-        .map(|request: WebhookRequest| {
-            match parse_mac_address(&request.mac) {
-                Ok(mac) => {
-                    info!("Received request to send WoL packet to MAC: {}", request.mac);
-                    if let Err(e) = send_wol_packet(mac) {
-                        error!("Failed to send WoL packet: {}", e);
-                        warp::reply::with_status("Failed to send WoL packet", warp::http::StatusCode::INTERNAL_SERVER_ERROR)
-                    } else {
-                        warp::reply::with_status("WoL packet sent", warp::http::StatusCode::OK)
-                    }
-                }
-                Err(e) => {
-                    error!("Invalid MAC address format: {}", e);
-                    warp::reply::with_status("Invalid MAC address format", warp::http::StatusCode::BAD_REQUEST)
-                },
-            }
-        });
+    let state = Arc::new(AppState {
+        machine_status: Mutex::new(HashMap::new()),
+        broadcast_ip,
+        broadcast_port,
+        username,
+        password,
+        token,
+    });
 
-    warp::serve(webhook)
-        .run(([0, 0, 0, 0], 8080))  // Utiliser le port 8080
-        .await;
-}
+    let make_svc = make_service_fn(move |_| {
+        let state = state.clone();
+        async move { Ok::<_, hyper::Error>(service_fn(move |req| handle_request(req, state.clone()))) }
+    });
 
-fn parse_mac_address(mac_str: &str) -> Result<[u8; 6], &'static str> {
-    let bytes: Vec<u8> = mac_str.split(':')
-        .map(|s| u8::from_str_radix(s, 16))
-        .collect::<Result<Vec<_>, _>>()
-        .map_err(|_| "Invalid MAC address format")?;
-    
-    if bytes.len() == 6 {
-        let mut mac = [0u8; 6];
-        mac.copy_from_slice(&bytes);
-        Ok(mac)
-    } else {
-        Err("Invalid MAC address length")
+    let addr = SocketAddr::from(([0, 0, 0, 0], port));
+    let server = Server::bind(&addr).serve(make_svc);
+
+    println!("Listening for requests on port {}", port);
+
+    if let Err(e) = server.await {
+        eprintln!("Server error: {}", e);
     }
-}
-
-fn send_wol_packet(mac: [u8; 6]) -> Result<(), String> {
-    let mut packet = [0u8; 102];
-    for i in 0..6 {
-        packet[i] = 0xFF;
-    }
-    for i in 0..16 {
-        for j in 0..6 {
-            packet[6 + i * 6 + j] = mac[j];
-        }
-    }
-
-    let socket = UdpSocket::bind("0.0.0.0:0").map_err(|e| e.to_string())?;
-    socket.set_broadcast(true).map_err(|e| e.to_string())?;
-    socket.send_to(&packet, "255.255.255.255:9").map_err(|e| e.to_string())?;
-    
-    Ok(())
 }
